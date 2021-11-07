32589aa48e39e4f72989675d856d34b44bf3da26
VertAdam <vertadam@gmail.com>
2021-11-07 18:38:12 -0500

    Initialize Network File

---
+++ networks.py
@@ -0,0 +1,28 @@
+#################################################### Nov 11, 2021 ####################################################
+"""
+Local Global and Combined Neural Network Architecture
+"""
+#################################################### Imports ####################################################
+import numpy as np
+import torch.nn as nn
+import torch
+
+
+#################################################### Code ####################################################
+
+# Local View
+
+def LocalNetwork(nn.Module)
+
+
+
+if __name__ == "__main__":
+    # Load fake data TODO: Get Real Data
+    local_views = np.random.random((1000, 51))
+    global_views = np.random.random((1000, 701))
+    impact = np.random.random(1000)
+    rad_rat = np.random.random(1000)
+
+
+
+x = 1
